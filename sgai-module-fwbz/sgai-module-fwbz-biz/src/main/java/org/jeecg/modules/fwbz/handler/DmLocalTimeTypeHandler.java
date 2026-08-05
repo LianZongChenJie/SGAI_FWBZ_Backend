88a9e4b879ce27d725ec5e919ca4f30603e5dbee
selionce <jacktoolove@126.com>
2026-08-05 10:07:00 +0800

    修复bug

--- sgai-module-fwbz/sgai-module-fwbz-biz/src/main/java/org/jeecg/modules/fwbz/handler/DmLocalTimeTypeHandler.java
+++ sgai-module-fwbz/sgai-module-fwbz-biz/src/main/java/org/jeecg/modules/fwbz/handler/DmLocalTimeTypeHandler.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.MappedTypes;
-import org.jeecg.modules.fwbz.project.dto.EvaluationReportQueryDto;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
