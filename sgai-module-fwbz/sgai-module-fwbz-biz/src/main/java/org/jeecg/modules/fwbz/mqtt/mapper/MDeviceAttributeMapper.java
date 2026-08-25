5fb07f5a7b0007415a3b2918596f1fa2b1d2a001
selionce <jacktoolove@126.com>
2026-08-25 16:19:44 +0800

    feat(fwbz): 添加按采集编码批量更新设备属性

--- sgai-module-fwbz/sgai-module-fwbz-biz/src/main/java/org/jeecg/modules/fwbz/mqtt/mapper/MDeviceAttributeMapper.java
+++ sgai-module-fwbz/sgai-module-fwbz-biz/src/main/java/org/jeecg/modules/fwbz/mqtt/mapper/MDeviceAttributeMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Update;
 import org.jeecg.modules.fwbz.mqtt.entity.DeviceAttribute;
 import org.jeecg.modules.fwbz.mqtt.entity.MqttHistory;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -41,4 +42,32 @@ public interface MDeviceAttributeMapper extends BaseMapper<DeviceAttribute> {
             "</foreach>" +
             "</script>")
     int updateValueByUniqueKeys(@Param("list") List<MqttHistory> list);
+
+    /**
+     * 按采集编码(acquisition_coding) 批量更新采集值和采集时间
+     * 一条 SQL 更新多行，减少数据库交互次数（用于楼控批量读点后统一落库）
+     *
+     * @param list 待更新数据（需包含 acquisitionCoding、value、gatherTime 字段）
+     * @return 影响行数
+     */
+    @Update("<script>" +
+            "UPDATE device_attribute " +
+            "<set>" +
+            "value = CASE acquisition_coding " +
+            "<foreach collection='list' item='item'>" +
+            "WHEN #{item.acquisitionCoding} THEN #{item.value} " +
+            "</foreach>" +
+            "END, " +
+            "gather_time = CASE acquisition_coding " +
+            "<foreach collection='list' item='item'>" +
+            "WHEN #{item.acquisitionCoding} THEN #{item.gatherTime} " +
+            "</foreach>" +
+            "END " +
+            "</set>" +
+            "WHERE acquisition_coding IN " +
+            "<foreach collection='list' item='item' open='(' separator=',' close=')'>" +
+            "#{item.acquisitionCoding}" +
+            "</foreach>" +
+            "</script>")
+    int updateValueByIds(@Param("list") Collection<org.jeecg.modules.fwbz.mdm.entity.DeviceAttribute> list);
 }
